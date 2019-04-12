b93ea18dd0c55bae87cf01f555cd15aaf62ee275
Astraroth <aresse@et.esiea.fr>
2019-04-12 23:31:55 +0200

    Navigation 2 écrans+ ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { StyleSheet, Dimensions, Text, View, Button,Image,TouchableOpacity, Animated} from 'react-native';
-import Search from './Components/Search'
+import { StyleSheet, Dimensions, ScrollView, Text, View, Button,Image,TouchableOpacity, Animated} from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+//import Search from './Components/Search'
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -14,58 +21,94 @@ const Assets = [
   { id: "6", uri: require('./assets/FakePicture/6.jpg')},
   { id: "7", uri: require('./assets/FakePicture/7.jpg')},
 ]
-
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
 
   constructor() {
     super();
     global.MyVar = 0;
-    this.state = {
-    textValue : 'Text1'
-  }
+    
   }
 
   onPress = () => {
     var str1 = 'Test'
     var cpt = 5
     MyVar += 1
-    if(MyVar > 6)
+    if(MyVar > 6) //boucler sur les images
     {
       MyVar = 0
     }
     this.setState({
-      textValue : str1+" "+MyVar
+      
     })
   }
 
   render() {
     return (
-      <View style={{marginTop: 30, flex: 1}}>
 
-        <View style={{ flex: 4, justifyContent: "center", alignItems: "center"}}>
-          <Animated.View style={{ height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10}}>
+      
+      <ScrollView>
 
-          <Image style={{ flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 20}} source={Assets[MyVar].uri} />
+        <View style={{marginTop: 0, flex: 1}}>
 
-          </Animated.View>  
-        </View>
-        <View style={{ flex: 1,flexDirection: "row",backgroundColor: 'black' }}>
-          <View style={{ flex: 1, backgroundColor: 'red', justifyContent: "center", alignItems: "center" }}>
-            <TouchableOpacity activeOpacity = { .5 } onPress={ this.onPress }>
-              <Image style={{width: 100, height: 100}} source={require('./assets/unhappy.png')} />
-            </TouchableOpacity>
+          <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+          <Button
+            title="Go :)"
+            onPress={() => this.props.navigation.navigate('Details')}
+          />
           </View>
-          <View style={{ flex: 1, backgroundColor: 'blue', justifyContent: "center", alignItems: "center" }}>
-            <TouchableOpacity activeOpacity = { .5 } onPress={ this.onPress }>
-              <Image style={{width: 100, height: 100}} source={require('./assets/happy.png')} />
-            </TouchableOpacity>
+
+          <View style={{ flex: 7, justifyContent: "center", alignItems: "center"}}>
+            <Animated.View style={{ height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10}}>
+
+            <Image style={{ flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 20}} source={Assets[MyVar].uri} />
+
+            </Animated.View>  
+          </View>
+
+          <View style={{ flex: 1,flexDirection: "row",backgroundColor: 'black' }}>
+
+            <View style={{ flex: 1, backgroundColor: 'red', justifyContent: "center", alignItems: "center" }}>
+              <TouchableOpacity activeOpacity = { .5 } onPress={ this.onPress }>
+                <Image style={{width: 100, height: 100}} source={require('./assets/unhappy.png')} />
+              </TouchableOpacity>
+            </View>
+
+            <View style={{ flex: 1, backgroundColor: 'blue', justifyContent: "center", alignItems: "center" }}>
+              <TouchableOpacity activeOpacity = { .5 } onPress={ this.onPress }>
+                <Image style={{width: 100, height: 100}} source={require('./assets/happy.png')} />
+              </TouchableOpacity>
+            </View>            
           </View>
         </View>
 
+      </ScrollView>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Ceci est le détail !</Text>
+        
       </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
 
 const styles = StyleSheet.create({
   container: {
